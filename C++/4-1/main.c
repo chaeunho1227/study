0fd987b2871eb6ee07564cd12492350eb20e74ac
chaeunho1227 <chaeunho2002@gmail.com>
2023-04-03 18:00:33 +0900

    C++ 4-1

---
+++ C++/4-1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  4-1
+//
+//  Created by 차은호 on 2023/04/03.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
